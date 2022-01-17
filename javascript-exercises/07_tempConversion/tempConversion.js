cdf5cf017500f271b4aa0c5681de3d6d59bc4991
Dean Wagner <me@deanwagner.net>
2022-01-17 14:33:33 -0800

    javascript exercises 7 & 8

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return round((temp - 32) * 5 / 9);
+  
 };
 
-const ctof = function() {
+const ctof = function(temp) {
+  return round(temp * 9 / 5 + 32);
+};
 
+const round = function(num) {
+  return +(Math.round(num + "e+1")  + "e-1");
 };
 
 // Do not edit below this line
--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(data) {
+  let result = 0;
+  data.forEach(value => {
+    result += value;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(data) {
+  let result = 0;
+  data.forEach((value, index) => {
+    if (index === 0) {
+      result = value;
+    } else {
+      result *= value;
+    }
+  });
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let result = 0;
+  for (let i = 0; i < num2; i++) {
+    if (i === 0) {
+      result = num1;
+    } else {
+      result *= num1;
+    }
+  }
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    num = 1;
+  } else {
+    for (let i = num - 1; i >= 1; i--) {
+      num *= i;
+    }
+  }
+  return num;
 };
 
 // Do not edit below this line
