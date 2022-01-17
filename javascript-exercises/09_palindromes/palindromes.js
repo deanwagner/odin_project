5b4f5eabf762c1baf3c2391672efce16294844df
Dean Wagner <me@deanwagner.net>
2022-01-17 15:36:57 -0800

    javascript exercises 9 & 10

--- javascript-exercises/09_palindromes/palindromes.js
+++ javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let forward = str.replaceAll(" ", "").replaceAll("!", "").replaceAll(".", "").replaceAll(",", "").toLowerCase();
+    let backward = forward.split("").reverse().join("");
+    if (forward === backward) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const key = parseInt(num) - 1;
+    if (key >= 0) {
+        const fib = [1, 1];
 
+        for (let i = 2; i <= key; i++) {
+            fib[i] = fib[i - 2] + fib[i - 1];
+        }
+
+        return fib[key];
+    } else {
+        return "OOPS";
+    }
 };
 
 // Do not edit below this line
