b1a4953a7d2637272c12c1ccad0c63a0481b35eb
Dean Wagner <me@deanwagner.net>
2022-01-16 18:54:57 -0800

    javascript exercises 1 & 2

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let fin = "";
+    if (num >= 0) {
+        for (let i = 0; i < num; i++) {
+            fin += str;
+        }
+    } else {
+        fin = "ERROR";
+    }
+    return fin;
 };
 
 // Do not edit below this line
