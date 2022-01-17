7ed13e0f2fecee215b63ed55035e1eeaa9adc683
Dean Wagner <me@deanwagner.net>
2022-01-17 12:58:39 -0800

    javascript exercises 5 & 6

--- javascript-exercises/05_sumAll/sumAll.js
+++ javascript-exercises/05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let result = "ERROR";
+    if ((checkNum(num1)) && (checkNum(num2))) {
+        if (num1 === num2) {
+            result = num1 + num2;
+        } else if (num1 < num2) {
+            result = calc(num1, num2);
+        } else {
+            result = calc(num2, num1);
+        }
+    }
+    return result;
+};
 
+const checkNum = function(num) {
+    switch (true) {
+        case (!Number.isInteger(num)):
+            return false;
+        case (num < 0):
+            return false;
+        default:
+            return true;
+    }
+};
+
+const calc = function(first, last) {
+    let sum = 0;
+    for (let i = first; i <= last; i++) {
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
--- javascript-exercises/06_leapYears/leapYears.js
+++ javascript-exercises/06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let result = false;
+    if ((year % 4) === 0) {
+        if ((year % 100) === 0) {
+            if ((year % 400) === 0) {
+                result = true;
+            }
+        } else {
+            result = true;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
