557ed2e51def8626411442957c117ef49b9813fc
Dean Wagner <me@deanwagner.net>
2022-01-17 11:42:31 -0800

    javascript exercises 3 & 4

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(data, ...remove) {
 
+    for (let i = 0; i < remove.length; i++) {
+        const index = data.indexOf(remove[i]);
+        if (index >= 0) {
+            data.splice(index, 1);
+        }
+    }
+    
+    return data;
 };
 
 // Do not edit below this line
