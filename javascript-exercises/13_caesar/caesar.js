0bc993f30c576715b7ef0020134fb1a3425c6db2
Dean Wagner <me@deanwagner.net>
2022-01-19 10:20:00 -0800

    edit index.html

--- javascript-exercises/13_caesar/caesar.js
+++ javascript-exercises/13_caesar/caesar.js
@@ -1,3 +1,10 @@
+/**
+ * Encrypts (+key) or Decrypts (-key) text string per "Caeser Cipher" technique
+ * @author  Dean Wagner <info@deanwagner.net>
+ * @param   {string} cipher - Text to encrypt/decrypt
+ * @param   {number} key    - The Encryption Key
+ * @returns {number} - The encrypted/decrypted text
+ */
 const caesar = function(cipher, key) {
     const uppercase = ["A","B","C","D","E","F","G","H","I","G","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
     const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","w","z"];
@@ -5,14 +12,18 @@ const caesar = function(cipher, key) {
     let encrypted = "";
     key = parseInt(key);
 
+    // Loop through each character in the cipher string
     character.forEach(char => {
         if (uppercase.includes(char)) {
+            // Uppercase Letter Match
             const index = uppercase.indexOf(char);
-            encrypted += uppercase[shiftKey(index, key)];
+            encrypted += uppercase[shiftFactor(index, key)];
         } else if (lowercase.includes(char)) {
+            // Lowercase Letter Match
             const index = lowercase.indexOf(char);
-            encrypted += lowercase[shiftKey(index, key)];
+            encrypted += lowercase[shiftFactor(index, key)];
         } else {
+            // Ignore
             encrypted += char;
         }
     });
@@ -20,11 +31,19 @@ const caesar = function(cipher, key) {
     return encrypted;
 };
 
-const shiftKey = function(index, key) {
+/**
+ * Helper Function for caesr(); Calculates shift for character index
+ * @author  Dean Wagner <info@deanwagner.net>
+ * @param   {number} index - Index of matched Character
+ * @param   {number} key   - Encryption Key
+ * @returns {number} - encrypted/decrypted text
+ */
+const shiftFactor = function(index, key) {
     let shift;
     let factor;
     let result;
 
+    // Calculate Factor
     if (key > 26) {
         let k = key;
         while (k > 26) { 
@@ -41,6 +60,7 @@ const shiftKey = function(index, key) {
         factor = key;
     }
 
+    // Calculate Shift Amount
     shift = index + factor;
 
     if (shift > 25) {
@@ -50,7 +70,7 @@ const shiftKey = function(index, key) {
     } else {
         result = shift;
     }
-    console.log(result);
+
     return result;
 }
 
