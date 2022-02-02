27823a083a3d225b0408f988974b855cd1f96e6f
Dean Wagner <me@deanwagner.net>
2022-02-01 20:54:25 -0800

    validation bug fix

--- signup/res/signup.js
+++ signup/res/signup.js
@@ -8,25 +8,14 @@ function handleForm() {
     const form   = document.getElementsByTagName('form')[0];
     const button = form.getElementsByTagName('button')[0];
     const inputs = document.getElementsByTagName('input');
+    const pass1  = document.getElementById('password-one');
+    const pass2  = document.getElementById('password-two');
 
     // Button Click Event Listener
     button.addEventListener('click', () => {
         // Validate All Inputs
         form.classList.add('validate');
-        messages();
-    });
 
-    // Input Blur Event Listener
-    for (let i = 0; i < inputs.length; i++) {
-        inputs[i].addEventListener('blur', (e) => {
-            // Validate this Input
-            e.target.classList.add('validate');
-            messages();
-        });
-    }
-
-    // Show/Hide Error Messages
-    function messages() {
         // Remove Old Messages
         const valid = form.querySelectorAll('input:valid');
         for (let i = 0; i < valid.length; i++) {
@@ -42,12 +31,47 @@ function handleForm() {
         for (let i = 0; i < invalid.length; i++) {
             const label = invalid[i].parentElement;
             const div   = label.getElementsByTagName('div')[0];
-            console.log(div);
             if (typeof div === 'undefined') {
                 const error = document.createElement('div');
                 error.innerText = label.title;
                 label.appendChild(error);
             }
         }
+
+        // Match Passwords
+        if (pass1.value !== pass2.value) {
+            pass2.setCustomValidity('Passwords Must Match');
+            pass2.reportValidity();
+        } else {
+            pass2.setCustomValidity('');
+        }
+    });
+
+    // Input Blur Event Listener
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('blur', (e) => {
+            // Validate this Input
+            e.target.classList.add('validate');
+            const label = inputs[i].parentElement;
+            const div   = label.getElementsByTagName('div')[0];
+            if (e.target.validity.valid) {
+                // Remove Old Message
+                if (typeof div !== 'undefined') {
+                    label.removeChild(div);
+                }
+            } else {
+                // Create New Message
+                if (typeof div === 'undefined') {
+                    const error = document.createElement('div');
+                    error.innerText = label.title;
+                    label.appendChild(error);
+                }
+            }
+        });
     }
+
+    // Form Submit Event Listener
+    form.addEventListener('submit', () => {
+        alert('Form Submitted');
+    });
 }
